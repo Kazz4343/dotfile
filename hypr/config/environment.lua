db4518387967d5afc3bf6b71d67011ee7feb5475
kazz4343 <mgjb101@hotmail.com>
2026-08-05 13:59:24 +0700

    update environment, hyprland

--- hypr/config/environment.lua
+++ hypr/config/environment.lua
@@ -20,7 +20,7 @@ hl.env("XDG_SESSION_TYPE", "wayland")
 hl.env("XDG_SESSION_DESKTOP", "Hyprland")
 
 -- Qt
-hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1.25")
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
